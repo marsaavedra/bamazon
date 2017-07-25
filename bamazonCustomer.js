1b89166c6b4109c902e4dbd8eaf6bf22a85d8d3e
maria saavedra <mariasaavedra@marias-MacBook-Air.local>
2017-07-25 08:27:11 -0400

    add inventory w/o breaking code progression issue

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-//place the databae info into an empty array
+//place the database info into an empty array
 var emptyArray = [];
 
 connection.connect(function(err) {
@@ -28,12 +28,13 @@ connection.connect(function(err) {
     if (err) throw err;
     //console.log("Items for sale: ", res);
         
-    //creat a loop to sort through the object array...
+    //creat a loop to sort through the results (res or each row) array of objects
         for(var i =0; i<res.length; i++) {
+            //calling consolidatedData function below and having the for loop run through it allows us to loop through its properties (or column) of our Object called NewProduct (Product is our constructor). 
         consolidatedData(res[i]);
         }
+        //now that we have stored our data into our local machine by placing it into our empty array we can now use it all and begin using our info by calling the start function 
         start();
-       // console.log("empty Array: ", emptyArray);
         
     });
 });
@@ -51,30 +52,56 @@ function start () {
         message: "How many units of the product would you like to buy?"
         }
     ]).then(function(answer){
-        
+        //accessing our data from the empty array (which is running through our objects length)
         for(var i =0; i<emptyArray.length; i++) {
             
-            
+            //for a especific obejct in the array, grab its item ID and compare it to our answer that we inputed that specifies its name label "askID"...
             if(emptyArray[i].item_id === parseInt(answer.askID)) {
-            
-                console.log("emptyArray: ", emptyArray[i].product_name);
+                
+                //then take the number inputed from askUnits and subtract its from the units currently available in the emptyArray Object. 
+                var newQuantity = emptyArray[i].stock_quantity -= answer.askUnits;
                 //update database to display remaining quantities
+                //------Updating SQL----------
+                    
+                if(newQuantity>0) {
+                    var query = connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                          {
+                            stock_quantity: newQuantity
+                          },
+                          {
+                            item_id: emptyArray[i].item_id
+                          }
+                        ],
+                        function(err, res) {
+                          console.log(res.affectedRows + " products updated!\n");
+                          
+                        }
+                      );
+                //------End of SQL update-----
+                    //the tell user its available 
+                    console.log(emptyArray[i].product_name + " is available");
+                    //and tell user the total cost of their product which will be the price of item multiplies by the units the of the user's input.
+                    console.log("The total cost of your order is: $" + emptyArray[i].price* answer.askUnits);
+                }else {
+                    console.log("Insufficient quantity! Order cannot be completed");
+                }
+    
+                
                 //display total price of purchase to customers
-            }
-        }
-        
-        //if stock_quantity >0 then display sql databse to reflect new quantity
+                //if stock_quantity >0 then display sql databse to reflect new quantity
         
         //then show customer the total cost of his purchase
         //else tell customer there are not enough in stock
+            }
+        }
+        
+        
     });
 }
 
 
-//access the database
-//-----connection.query("SELECT * FROM products", function(err, res) {
-//    if (err) throw err;
-//    console.log("Items for sale: ", res);
 //create cosntructor to access the info in the array. 
 var Product = function (item_id, product_name, department_name, price, stock_quantity) {
     this.item_id  = item_id;
@@ -88,14 +115,16 @@ var Product = function (item_id, product_name, department_name, price, stock_qua
         
 function consolidatedData (res) {
     var item_id = res.item_id;
-    var product = res.product_name;
-    var department = res.department_name;
+    var product_name = res.product_name;
+    var department_name = res.department_name;
     var price = res.price;
-    var stock = res.stock_quantity;
+    var stock_quantity = res.stock_quantity;
     
-    var newProduct = new Product(item_id, product, department, price, stock);
+    var newProduct = new Product(item_id, product_name, department_name, price, stock_quantity);
     
+    //this new object called newProduct (which is our specific table) displays all its data which is now stored in our local machine in our array. 
     emptyArray.push(newProduct);
+
     
 }
         
---
+++ bamazonManager.js
@@ -0,0 +1,201 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var password = require("./password.js");
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: password,
+  database: "bamazon"
+});
+
+//place the database info into an empty array
+var emptyArray = [];
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+    
+    connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    //console.log("Items for sale: ", res);
+        
+    //creat a loop to sort through the results (res or each row) array of objects
+        for(var i =0; i<res.length; i++) {
+            //calling consolidatedData function below and having the for loop run through it allows us to loop through its properties (or columns) of our Object called NewProduct (Product is our constructor). 
+        consolidatedData(res[i]);
+        }
+        //now that we have stored our data into our local machine by placing it into our empty array we can now use it all and begin using our info by calling the start function 
+        start();
+        
+    });
+});
+
+
+function start () {
+    inquirer.prompt([
+        {
+        type: "list",
+			name: "menu",
+			message: "Please select an option",
+			choices: [
+				"View Products for Sale",
+				"View Low Inventory",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        }
+    ]).then(function(answer){
+        //accessing our data from the empty array (which is running through the lenght of our rows)
+            if(answer.menu === "View Products for Sale") {
+                console.log(emptyArray);
+            }
+            //must create a for loop that goes through all of the rows to determine their characterists
+                if(answer.menu === "View Low Inventory") {
+                    for(var i =0; i<emptyArray.length; i++) {
+                //display items with an inventory count lower than 5
+                    if(emptyArray[i].stock_quantity < 5) {
+                        console.log("Items with low inventory: " + emptyArray[i].product_name);
+                    }
+                    }
+                }
+                if(answer.menu === "Add to Inventory") {
+                    //Prompt user to add inventory (stock_quantity) to a current product by first typing the products id. 
+                    inquirer.prompt([
+                        {
+                        name: "askID",
+                        type: "input",
+                        message: "Type the ID number of the item you want to add inventory to"
+                        },
+                        {
+                        name: "askUnits",
+                        type: "input",
+                        message: "How many units of the product would you like to add to the inventory?"
+                        }
+                    ]).then(function(answer){
+                        for(var i =0; i<emptyArray.length; i++) {
+            
+                            if(emptyArray[i].item_id === parseInt(answer.askID)) {
+                                var newQuantity = emptyArray[i].stock_quantity += parseFloat(answer.askUnits);
+                            
+                                //update database to display remaining quantities
+                                //------Updating SQL----------
+                                    var query = connection.query(
+                                        "UPDATE products SET ? WHERE ?",
+                                        [
+                                          {
+                                            stock_quantity: newQuantity
+                                          },
+                                          {
+                                            item_id: emptyArray[i].item_id
+                                          }
+                                        ],
+                                        function(err, res) {
+                                          console.log(res.affectedRows + " products updated!\n");
+
+                                        }
+                                      );
+                                //------End of SQL update-----
+                                 console.log(answer.askUnits + " have been added to the "+ emptyArray[i].product_name + "inventory, for a total of " + newQuantity);  
+
+                            }
+                        }//-----end of for loop
+                    });
+                }//--end of if statement for "Add Inventory----
+                
+                if(answer.menu === "Add New Product") {
+                   function addProduct () { 
+                       inquirer.prompt([
+                        {
+                        name: "askID",
+                        type: "input",
+                        message: "Type the ID number of the item you want to add inventory to"
+                        },
+                        {
+                        name: "productName",
+                        type: "input",
+                        message: "What is the name of the product that you would like to add?"
+                        },
+                        {
+                        name: "departmentName",
+                        type: "input",
+                        message: "What is the name of the department of which you would like to add your product in?"
+                        },
+                        {
+                        name: "price",
+                        type: "input",
+                        message: "What is the price of the product?"
+                        },
+                        {
+                        name: "askUnits",
+                        type: "input",
+                        message: "How many units of the product would you like to add to the inventory?"
+                        }
+                       ])}.then(function(answer){
+                        for(var i=0; i< emptyArray.length; i++) {
+                            if(emptyArray[i] === parseInt(answer.askID)) {
+                                console.log("oops, Id name for product already exists, try again");
+                                addProduct();
+                            } else {
+                                addProduct();
+                                break;
+                            }
+                        }
+                    
+                     var query = connection.query(
+                        "INSERT INTO products SET ?",
+                        {
+                          item_id: answer.askID,
+                          product_name: answer.productName,
+                          department_name: answer.departmentName,
+                          price: answer.price,
+                          stock_quantity: answer.askUnits
+                        },
+                        function(err, res) {
+                          console.log(res.affectedRows + " product inserted!\n");
+                          console.log(answer.productName + "was added to the invetory");
+                          
+                            }
+                      
+                        )
+                    });
+                            
+           
+        
+    }//----end of add new product
+})//---end of giant promise for all of our options
+            };
+
+//create cosntructor to access the info in the array. 
+var Product = function (item_id, product_name, department_name, price, stock_quantity) {
+    this.item_id  = item_id;
+    this.product_name  = product_name;
+    this.department_name  = department_name;
+    this.price = price;
+    this.stock_quantity  = stock_quantity;
+    
+    
+};
+        
+function consolidatedData (res) {
+    var item_id = res.item_id;
+    var product_name = res.product_name;
+    var department_name = res.department_name;
+    var price = res.price;
+    var stock_quantity = res.stock_quantity;
+    
+    var newProduct = new Product(item_id, product_name, department_name, price, stock_quantity);
+    
+    //this new object called newProduct (which is our specific table) displays all its data which is now stored in our local machine in our array. 
+    emptyArray.push(newProduct);
+
+    
+}
